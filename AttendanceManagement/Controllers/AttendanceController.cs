9b635adb21c7074bb0278a68158684237246c479
Admin <Admin@DESKTOP-H2SNSOG>
2023-08-30 17:52:42 +0530

    Add project files.

---
+++ AttendanceManagement/Controllers/AttendanceController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttendanceManagement.Controllers
+{
+    public class AttendanceController : Controller 
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AttendanceIndex()
+        {
+            return View();
+        }
+        public IActionResult ReportIndex()
+        {
+            return View();
+        }
+        public IActionResult AddEmployeeIndex()
+        {
+            return View();
+        }
+
+    }
+}
